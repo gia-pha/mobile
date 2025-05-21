731a92261bf93470187daf46981c1136c218110a
tien.xuan.vo <tien.xuan.vo@gmail.com>
2025-05-22 06:55:05 +0700

    feat: Add members tab

---
+++ lib/utils/EADataProvider.dart
@@ -0,0 +1,90 @@
+import 'package:gia_pha_mobile/model/EAForYouModel.dart';
+
+List<EAForYouModel> getMayKnowData() {
+  List<EAForYouModel> list = [];
+  list.add(EAForYouModel(
+      name: 'jose Lowe',
+      add: '156 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.9.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'Smit Jhon',
+      add: '200 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.1.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'Louisa Lyons',
+      add: '230 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.2.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'Hulda James',
+      add: '100 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.3.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'Bessie Mendoza',
+      add: '150 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.4.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'Matilda MCGuire',
+      add: '260 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.9.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'Harriett Coleman',
+      add: '400 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.1.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'jose Lowe',
+      add: '156 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.9.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'Smit Jhon',
+      add: '200 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.1.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'Louisa Lyons',
+      add: '230 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.2.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'Hulda James',
+      add: '100 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.3.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'Bessie Mendoza',
+      add: '150 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.4.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'Matilda MCGuire',
+      add: '260 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.9.jpg',
+      fev: false));
+  list.add(EAForYouModel(
+      name: 'Harriett Coleman',
+      add: '400 followers',
+      image:
+          'https://assets.iqonic.design/old-themeforest-images/prokit/datingApp/Image.1.jpg',
+      fev: false));
+  return list;
+}
