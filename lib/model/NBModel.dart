1afeaadc8585946ca94689ea5253b6606c750f45
tien.xuan.vo <tien.xuan.vo@gmail.com>
2025-12-01 03:13:04 +0700

    chore: Clean up old screens

--- lib/model/NBModel.dart
+++ lib/model/NBModel.dart
@@ -1,26 +1,7 @@
-class NBNewsDetailsModel {
-  String? categoryName;
-  String? title;
-  String? date;
-  String? image;
-  String? details;
-  bool isBookmark;
-  String? time;
-
-  NBNewsDetailsModel({this.categoryName, this.title, this.date, this.image, this.details, this.isBookmark = false, this.time});
-}
 
 class NBLanguageItemModel {
   String image;
   String name;
 
   NBLanguageItemModel(this.image, this.name);
-}
-
-class NBMembershipPlanItemModel {
-  String timePeriod;
-  String price;
-  String text;
-
-  NBMembershipPlanItemModel(this.timePeriod, this.price, this.text);
 }
